3a67a136666cef21a088fb92079cc43d430fab3b
Renat Zaripov <renat.zaripov@elev.ga.ntig.se>
2024-11-06 15:06:39 +0100

    first commit

---
+++ If-satser, loopar/Program.cs
@@ -0,0 +1,49 @@
+﻿// if (6 > 3)
+// {
+//     Console.WriteLine("Hello World");
+
+// }
+// Console.ReadLine();
+
+// ---------------------------------------------------
+
+// Console.WriteLine("Hej, skriv ditt användranamn:");
+
+// string namn = Console.ReadLine();
+
+// if (namn == "noname")
+// {
+//     Console.WriteLine("Wellcome!");
+// }
+
+// else
+// {
+//     Console.WriteLine("Det är fel!");
+// }
+// Console.ReadLine();
+// ----------------------------------------------------
+// string namn = "noname";
+// string lösenord = "nopass";
+// Console.WriteLine("Hej, skriv ditt användranamn:");
+// string svar1 = Console.ReadLine();
+// Console.WriteLine("Trevligt, skriv ditt lösenord:");
+// string svar2 = Console.ReadLine();
+// if (namn == svar1 && lösenord == svar2)
+// {
+//     Console.WriteLine("Welcome!");
+// }
+// else
+// {
+//     Console.WriteLine("Wrong username or password");
+// }
+
+// Console.ReadLine();
+// -----------------------------------------------------
+int i = 0;
+        while (i < 32)
+        {
+            Console.WriteLine("Hello, World");
+            i++;
+        }
+        Console.ReadLine();
+
